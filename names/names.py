cfb6f88f3d884753ef059b9777cab6ba8e83e4d4
carlsachs <sachscarl@gmail.com>
2020-07-17 12:38:31 -0400

    finished names, and added explanations throughout files

--- names/names.py
+++ names/names.py
@@ -13,13 +13,15 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 # grab the first name to start tree
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+bst = BSTNode(names_1[0])
+# insert the rest from first file into the tree
+for name in names_1[1:]:
+    bst.insert(name)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
 
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,13 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
+        #set a store as a list with nothing in it times whatever is in capacity
         self.store = [None] * capacity
+        #set the element equal to 0
         self.old_element = 0
     def append(self, item):
+        #set the store with the old element to variable item
         self.store[self.old_element] = item
+        #if theres nothing in the old element, it equals 0
         if self.old_element == self.capacity -1:
             self.old_element = 0
         else:
+            #else incredment one
             self.old_element += 1
     def get(self):
+        # return each item in the self store/old element if the item is not None
         return [item for item in self.store if item is not None]
